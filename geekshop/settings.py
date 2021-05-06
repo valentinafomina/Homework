188d026465c193f3d67d423d4980c41b59f94a4e
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-05-06 19:31:09 +0300

    Add files via upload

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -120,10 +120,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "geekshop", "static"),
+    os.path.join(BASE_DIR, "geekshop", "mainapp", "static"),
 )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
 from mainapp import urls
 
 urlpatterns = [
@@ -23,3 +25,6 @@ urlpatterns = [
 ]
 
 app_name = "geekshop"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
