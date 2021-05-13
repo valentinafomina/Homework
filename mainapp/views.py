fa13177fc1f4472e94744348c091561e7df2adb7
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-05-13 16:10:16 +0300

    Add files via upload

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
 
 
 def main(request):
@@ -13,16 +14,38 @@ def main(request):
     return render(request, 'mainapp/index.html', context=context)
 
 
-def products(request):
+def products(request, pk=None):
+    print(pk)
 
-    links_menu = {'links': [
-            {'href': 'mainapp:index', 'name': 'все'},
-            {'href': 'mainapp:index', 'name': 'дом'},
-            {'href': 'mainapp:index', 'name': 'офис'},
-            {'href': 'mainapp:index', 'name': 'модерн'},
-            {'href': 'mainapp:index', 'name': 'классика'}
-        ]}
-    return render(request, 'mainapp/products.html', context=links_menu)
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+
+    return render(request, 'mainapp/products.html', content)
 
 
 def contacts(request):
