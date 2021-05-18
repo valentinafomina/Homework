d26af65ecf8ad1d5c5c9bef2fb597ff834f89dfb
valentinafomina <69861285+valentinafomina@users.noreply.github.com>
2021-05-18 19:59:03 +0300

    Add files via upload

--- authapp/views.py
+++ authapp/views.py
@@ -5,9 +5,11 @@ from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
-    title = '????'
+    title = 'Вход'
 
     login_form = ShopUserLoginForm(data=request.POST)
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
+
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
@@ -15,9 +17,16 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('mainapp:index'))
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('mainapp:index'))
 
-    context = {'title': title, 'login_form':login_form}
+    context = {
+        'title': title,
+        'login_form':login_form,
+        'next' : next
+    }
     return render(request, 'authapp/login.html', context=context)
 
 
